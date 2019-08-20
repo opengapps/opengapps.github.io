d598f6e3e5ef02f4d7ba1d1a37f63ebfad0d9da1
Ilya Danilkin <nezorflame@gmail.com>
2019-08-20 18:44:44 +0300

    WIP

--- _includes/appscripts.js
+++ _includes/appscripts.js
@@ -1,12 +1,26 @@
-document.addEventListener('DOMContentLoaded',function(){window.componentHandler.upgradeDom();try{(adsbygoogle=window.adsbygoogle||[]).push({google_ad_client: "ca-pub-9489060368971640",enable_page_level_ads:true})}catch(e){};for(var iAd=0;iAd<document.getElementsByClassName('adsbygoogle').length;iAd++){try{(window.adsbygoogle||[]).push({});}catch(e){}}});
+document.addEventListener('DOMContentLoaded', function () {
+    window
+        .componentHandler
+        .upgradeDom();
+    try {
+        (adsbygoogle = window.adsbygoogle || []).push({google_ad_client: 'ca-pub-9489060368971640', enable_page_level_ads: true})
+    } catch (e) {};
+    for (var iAd = 0; iAd < document.getElementsByClassName('adsbygoogle').length; iAd++) {
+        try {
+            (window.adsbygoogle || []).push({});
+        } catch (e) {}
+    }
+});
 var dialog = document.querySelector('dialog');
 var showDialogButton = document.querySelector('#show-dialog');
-if (! dialog.showModal) {
-  dialogPolyfill.registerDialog(dialog);
+if (!dialog.showModal) {
+    dialogPolyfill.registerDialog(dialog);
 }
-showDialogButton.addEventListener('click', function() {
-  dialog.showModal();
+showDialogButton.addEventListener('click', function () {
+    dialog.showModal();
 });
-dialog.querySelector('.close').addEventListener('click', function() {
-  dialog.close();
-});
+dialog
+    .querySelector('.close')
+    .addEventListener('click', function () {
+        dialog.close();
+    });
--- _includes/blogscripts.js
+++ _includes/blogscripts.js
@@ -1,1 +1,13 @@
-document.addEventListener('DOMContentLoaded',function(){window.componentHandler.upgradeDom();try{(adsbygoogle=window.adsbygoogle||[]).push({google_ad_client: "ca-pub-9489060368971640",enable_page_level_ads:true})}catch(e){};for(var iAd=0;iAd<document.getElementsByClassName('adsbygoogle').length;iAd++){try{(window.adsbygoogle||[]).push({});}catch(e){}}});
+document.addEventListener('DOMContentLoaded', function () {
+    window
+        .componentHandler
+        .upgradeDom();
+    try {
+        (adsbygoogle = window.adsbygoogle || []).push({google_ad_client: 'ca-pub-9489060368971640', enable_page_level_ads: true})
+    } catch (e) {};
+    for (var iAd = 0; iAd < document.getElementsByClassName('adsbygoogle').length; iAd++) {
+        try {
+            (window.adsbygoogle || []).push({});
+        } catch (e) {}
+    }
+});
